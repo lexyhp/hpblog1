dd122ad0dedda20c43988a3d92067ef796210377
lexyhp <389232612@qq.com>
2017-06-02 11:50:26 +0800

    Update wsgi.py

--- hpblog1/wsgi.py
+++ hpblog1/wsgi.py
@@ -10,9 +10,7 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-path = '/home/lexhp/sites/hpblog1'
-if path not in sys.path:
-    sys.path.append(path)
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hpblog1.settings")
 
 application = get_wsgi_application()
